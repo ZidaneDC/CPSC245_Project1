9f5bb21f539cc0d5662bcd1def2bc5789215310c
delmt <ohcmonnow29@gmail.com>
2021-02-12 10:07:36 -0800

    character script added

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Collider dragonCollider;
+    private int charaacterHappiness;
+    private int characterEnergy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"):
+            shootFire();
+
+    }
+
+    private void shootFire()
+    {
+        void OnMouseOver()
+        {
+            //Target.Hit();
+        }
+    }
+
+    private void jump()
+    {
+        //button press for jump
+        //character moves a certain height up
+    }
+
+    private void duck()
+    {
+        //button press for duck
+        //character has duck animation
+    }
+}
